2692905be8f6978ec6deeb1ca180cafbf4fa3bdc
System Administrator <root@adsl-172-10-100-78.dsl.hstntx.sbcglobal.net>
2017-12-02 01:13:40 -0200

    Added backend start

---
+++ back-end/model.py
@@ -0,0 +1,58 @@
+import sqlalchemy
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy import Column, Integer, String, Boolean, Date, DateTime, Float
+from sqlalchemy import ForeignKey
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker, relationship
+from sqlalchemy.sql import func
+from sqlalchemy import LargeBinary
+
+
+Base = declarative_base()
+
+class Ocorrencia(Base):
+	__tablename__ = 'Ocorrencia'
+	id = Column(Integer, primary_key=True)
+	telefone = Column(Integer)
+	solicitante = Column(String)
+	municipio = Column(String)
+	endereco = Column(String)
+	numero = Column(Integer)
+	bairro = Column(String)
+	referencia = Column(String)
+	paciente = Column(String)
+	sexo = Column(String)
+	idade = Column(Integer)
+	queixa = Column(String)
+	observacoes = Column(String)
+	emergencia = Column(Boolean)
+	status = Column(String)
+
+class Media(Base):
+	__tablename__ = 'Media'
+	id = Column(Integer, primary_key=True)
+	ocorrencia_id = Column(Integer, ForeignKey('Ocorrencia.id'))
+	binary = Column(LargeBinary)
+
+
+class Recurso(Base):
+	__tablename__ = 'Recurso'
+	id = Column(Integer, primary_key=True)
+	status = Column(String)
+	localizacao = Column(String)
+	tipo = Column(String)
+    atendendo = Column(Integer, ForeignKey('Ocorrencia.id'))
+
+class Sinais_Vitais(Base):
+	__tablename__ = 'Sinais_Vitais'
+	id = Column(Integer, primary_key=True)
+	id_ocorrencia = Column(Integer, ForeignKey('Ocorrencia.id'))
+	pressao = Column(String)
+	frequencia_cardiaca = Column(String)
+	saturacao_oxigenio = Column(String)
+	temperatura = Column(String)
+
+
+engine = create_engine('postgresql://postgres:postgres@localhost/postgres')
+Session = sessionmaker(bind=engine)
+Base.metadata.create_all(engine)
